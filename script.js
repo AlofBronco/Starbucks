4c7165323fc008cb7b7bd9206195803492a48e30
Alof Bronco <130389810+AlofBronco@users.noreply.github.com>
2024-10-20 18:37:27 +0300

    making 1020px animation

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.info-box-inner').forEach((infoBox) => {
+  infoBox.addEventListener('click', function () {
+    const unorderedList = this.querySelector('ul');
+
+    if (unorderedList.style.maxHeight) {
+      unorderedList.style.maxHeight = null;
+    } else {
+      unorderedList.style.maxHeight = unorderedList.scrollHeight + 'px';
+    }
+
+    const arrowSvg = this.querySelector('.arrow');
+    arrowSvg.classList.add('flip');
+  });
+});
