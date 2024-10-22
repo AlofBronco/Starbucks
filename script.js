b34f6bdce5fa7f7826355b7fa2e31e34d381d8f7
Alof Bronco <130389810+AlofBronco@users.noreply.github.com>
2024-10-22 20:24:09 +0300

    finished 780px adaptive

--- script.js
+++ script.js
@@ -13,25 +13,30 @@ document.querySelectorAll('.info-box-inner').forEach((infoBox) => {
   });
 });
 
-function showMenu() {
-  const menu = document.getElementById('slide-in-menu');
-  const burgerButton = document.getElementById('burger-button');
-  const body = document.getElementsByTagName('body');
+const menu = document.getElementById('slide-in-menu');
+const overlay = document.getElementById('overlay');
+const burgerButton = document.getElementById('burger-button');
+const body = document.getElementsByTagName('body')[0];
 
+function showMenu() {
   menu.style.right = '0';
-  body[0].style.overflow = 'hidden';
+  body.style.overflow = 'hidden';
+  setTimeout(() => {
+    overlay.style.opacity = '1';
+  }, 0);
+  overlay.style.display = 'block';
 
   burgerButton.toggleAttribute('onclick', 'showMenu()');
   burgerButton.setAttribute('onclick', 'closeMenu()');
 }
 
 function closeMenu() {
-  const menu = document.getElementById('slide-in-menu');
-  const burgerButton = document.getElementById('burger-button');
-  const body = document.getElementsByTagName('body');
-
   menu.style.right = '-600px';
-  body[0].style.overflow = 'visible';
+  body.style.overflow = 'visible';
+  overlay.style.opacity = '0';
+  setTimeout(() => {
+    overlay.style.display = 'none';
+  }, 200);
 
   burgerButton.toggleAttribute('onclick', 'closeMenu()');
   burgerButton.setAttribute('onclick', 'showMenu()');
